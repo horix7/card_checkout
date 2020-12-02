4cbfda99b4196bd88d9c06d8901f97c8e3af8c20
dushime J.pierre <horix77777@gmail.com>
2020-12-02 02:08:21 -0800

    Modified

---
+++ src/components/forms/checkout.jsx
@@ -0,0 +1,12 @@
+import React, {component, Fragment} from 'react'
+
+export default class MainNav extends Components  {
+
+    render( ) {
+        return (
+            <Fragment>
+                
+            </Fragment>
+        )
+    }
+}
