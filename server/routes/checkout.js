e2d05532867a53b469bffec1c848dd541061508c
dushime J.pierre <horix77777@gmail.com>
2020-12-05 00:17:37 -0800

    modififed

---
+++ server/routes/checkout.js
@@ -0,0 +1,35 @@
+
+const express = require('express');
+const router = express.Router();
+const braintree = require('braintree');
+
+router.post('/', (req, res, next) => {
+  const gateway = new braintree.BraintreeGateway({
+    environment: braintree.Environment.Sandbox,
+    // Use your own credentials from the sandbox Control Panel here
+    merchantId: 'wmh3pppbk7ftf2kn',
+    publicKey: '63hzgmzqyvzp2zyj',
+    privateKey: 'f40d4dd9cffa37a24ccebb574c9b6422'
+  });
+
+  // Use the payment method nonce here
+  const nonceFromTheClient = req.body.paymentMethodNonce;
+  // Create a new transaction for $10
+  const newTransaction = gateway.transaction.sale({
+    amount: '10.00',
+    paymentMethodNonce: nonceFromTheClient,
+    options: {
+      // This option requests the funds from the transaction
+      // once it has been authorized successfully
+      submitForSettlement: true
+    }
+  }, (error, result) => {
+      if (result) {
+        res.send(result);
+      } else {
+        res.status(500).send(error);
+      }
+  });
+});
+
+module.exports = router;
